2ed0c300ef68db77afec676004f85481016eccea
Ahmad Alamoudi <amazing-boy@msn.com>
2020-08-14 18:52:08 +0300

    Update udac_example_dag.py

--- airflow/dags/udac_example_dag.py
+++ airflow/dags/udac_example_dag.py
@@ -11,8 +11,8 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'ahmad',
-    'start_date': datetime(2020, 8, 8),
-    'email': ['ahmad.amoudi@outlook.com'],
+    'start_date': datetime(2020, 8, 14),
+    'email': ['ahmad****@outlook.com'],
     'depends_on_past': False ,
     'retries': 3 ,
     'retry_delay': timedelta(minutes=5) ,
@@ -42,39 +42,62 @@ stage_events_to_redshift = StageToRedshiftOperator(
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    table="[public].staging_songs",
+    redshift_conn_id="redshift",
+    aws_credintials_id="aws_default",
+    s3_bucket="udacity-dend",
+    s3_key="song_data/A/A/A" ,
+    file_type="JSON 'auto'"
 )
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag,
+    table="[public].songplays",
     redshift_conn_id="redshift",
     sql_insert_query=SqlQueries.songplay_table_insert
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    table="[public].users",
+    redshift_conn_id="redshift",
+    sql_insert_query=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    table="[public].songs",
+    redshift_conn_id="redshift",
+    sql_insert_query=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    table="[public].artists",
+    redshift_conn_id="redshift",
+    sql_insert_query=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    table="[public].time",
+    redshift_conn_id="redshift",
+    sql_insert_query=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table=["songplays", "users", "songs", "artists", "time"],
+    test_query='SELECT COUNT(*) FROM songs WHERE songid is null;',
+    expected_result=0
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
