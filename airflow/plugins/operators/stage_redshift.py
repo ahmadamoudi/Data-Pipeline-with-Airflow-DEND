6c6a612686cf35aa9378e4f785890ef29d3c645c
Ahmad Alamoudi <amazing-boy@msn.com>
2020-08-17 21:50:48 +0300

    Update stage_redshift.py

--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -1,6 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
@@ -56,3 +57,4 @@ class StageToRedshiftOperator(BaseOperator):
         redshift.run(formatted_sql)
         
         self.log.info(f'Done: Copying {self.table} from S3 to Redshift')
+
